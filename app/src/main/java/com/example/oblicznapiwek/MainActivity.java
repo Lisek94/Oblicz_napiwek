468ab4b4a6f99067f4cae04a4ac2366668f2af66
Lisek94 <damian11800@gmail.com>
2020-02-24 20:59:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/oblicznapiwek/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.oblicznapiwek;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText amountEditText;
+    private TextView amountTextView;
+
+    private TextView percentTextView;
+    private SeekBar percentSeekBar;
+
+    private TextView tipLabelTextView;
+    private TextView tipTextView;
+
+    private TextView totalLabelTextView;
+    private TextView totalTextView;
+
+
+    private static final NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double tipPercent = 0.15;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amountEditText = findViewById(R.id.amountEditText);
+        amountTextView = findViewById(R.id.amountTextView);
+
+        percentTextView = findViewById(R.id.percentTextView);
+        percentSeekBar = findViewById(R.id.percentSeekBar);
+
+        tipLabelTextView = findViewById(R.id.tipLabelTextView);
+        tipTextView = findViewById(R.id.tipTextView);
+
+        totalLabelTextView = findViewById(R.id.totalLabelTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+
+        amountEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    billAmount = Double.parseDouble(s.toString()) / 100;
+                    amountTextView.setText(currencyFormat.format(billAmount));
+                } catch (NumberFormatException ex){
+                    amountTextView.setText("");
+                    billAmount = 0.0;
+                }
+
+                calculateTipAndTotalAmount();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        percentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tipPercent = progress / 100.0;
+                calculateTipAndTotalAmount();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void calculateTipAndTotalAmount(){
+
+        double tipAmount = billAmount*tipPercent;
+        double totalAmount = billAmount+tipAmount;
+
+        percentTextView.setText(percentFormat.format(tipPercent));
+        tipTextView.setText(currencyFormat.format(tipAmount));
+        totalTextView.setText(currencyFormat.format(totalAmount));
+    }
+}
